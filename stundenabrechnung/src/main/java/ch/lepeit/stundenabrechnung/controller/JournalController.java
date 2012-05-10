14ae1ce9f5fc1317bba9a08e90e8a7afa2db4699
Sven Tschui <info@eniu.ch>
2012-05-10 15:59:09 +0200

    (Unvollständiges) Journal hinzugefügt Zu Testzwecken auf h2 in Memory Datenbank umgestiegen

---
+++ stundenabrechnung/src/main/java/ch/lepeit/stundenabrechnung/controller/JournalController.java
@@ -0,0 +1,61 @@
+package ch.lepeit.stundenabrechnung.controller;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Vector;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import ch.lepeit.stundenabrechnung.model.Journal;
+
+@Named
+@SessionScoped
+public class JournalController implements Serializable {
+	private static final long serialVersionUID = 20120510L;
+
+	private Date woche = new Date();
+	
+	@Inject
+	private EntityManager em;
+	
+	public List<Date> getWochentage() {
+		List<Date> wochentage = new Vector<Date>();
+		
+		Calendar c = new GregorianCalendar();
+		
+		c.setTime(woche);
+		
+		c.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+		
+		for(int i = 0; i < 5; i++) {
+			wochentage.add(c.getTime());
+			c.add(Calendar.DAY_OF_MONTH, 1);
+		}
+		
+		return wochentage;
+	}
+	
+	public List<Journal> getBuchungen(Date tag) {
+		
+		TypedQuery<Journal> buchungen = em.createQuery("SELECT j FROM Journal j WHERE j.datum = :tag", Journal.class);
+		buchungen.setParameter("tag", tag);
+		
+		return buchungen.getResultList();
+	}
+
+	public Date getWoche() {
+		return woche;
+	}
+
+	public void setWoche(Date woche) {
+		this.woche = woche;
+	}
+	
+}
