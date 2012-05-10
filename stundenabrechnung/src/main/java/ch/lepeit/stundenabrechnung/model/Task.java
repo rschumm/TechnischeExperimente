b949c59c4db933f90480b322e905e072cf3a82e0
Sven Tschui <info@eniu.ch>
2012-05-10 16:24:52 +0200

    Organized imports

--- stundenabrechnung/src/main/java/ch/lepeit/stundenabrechnung/model/Task.java
+++ stundenabrechnung/src/main/java/ch/lepeit/stundenabrechnung/model/Task.java
@@ -2,7 +2,6 @@ package ch.lepeit.stundenabrechnung.model;
 
 import java.io.Serializable;
 import java.util.List;
-import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
